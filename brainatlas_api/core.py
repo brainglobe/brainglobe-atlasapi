b8e70bc3ef7b6db8351beb97262c5e46a8918be5
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-05-22 16:28:50 +0200

    Implementing Atlas class methods and mesh loading class

--- brainatlas_api/core.py
+++ brainatlas_api/core.py
@@ -1,8 +1,54 @@
-from .utils import open_json, read_tiff, make_hemispheres_stack
+from brainatlas_api.utils import open_json, read_tiff, make_hemispheres_stack
 from pathlib import Path
+from brainatlas_api.structure_tree import StructureTree
+from brainatlas_api.obj_utils import read_obj
+
+import pymesh
+
+
+class MeshDictionary(dict):
+    """ Class to cache the loading of .obj mesh files
+        Parameters
+        ----------
+        mesh_path : str or Path object
+            path to folder containing all meshes .obj files
+        """
+
+    def __init__(self, mesh_path):
+        self.root = Path(mesh_path)
+
+        # Create dictionary of loadable files:
+        self.files_dict = {int(f.resolve().stem): f for f in
+                           self.root.glob("*.obj")}
+
+    def __getitem__(self, item):
+        """ Load mesh if it has not been read before, and add it to self dict.
+        Parameters
+        ----------
+        item : int
+            id of structure to be loaded
+
+        Returns
+        -------
+        tuple :
+            Tuple with mesh description
+
+        """
+        if not item in self.keys():
+            value = read_obj(self.files_dict[item])
+            super().__setitem__(item, value)
+
+        return super().__getitem__(item)
 
 
 class Atlas():
+    """ Base class to handle atlases in brainglobe.
+        Parameters
+        ----------
+        path : str or Path object
+            path to folder containing data info
+        """
+
     def __init__(self, path):
         self.root = Path(path)
         self.metadata = open_json(self.root / "atlas_metadata.json")
@@ -10,11 +56,18 @@ class Atlas():
         for attr in ["name", "shape", "resolution"]:
             self.__setattr__(attr, self.metadata[attr])
 
-        self.structures = open_json(self.root / "structures.json")
+        structures_list = open_json(self.root / "structures.json")
+        self.structures = StructureTree(structures_list)
 
         self._reference = None
         self._annotated = None
         self._hemispheres = None
+        self.acronym_to_id_map = self.structures.get_id_acronym_map()
+
+        self.id_to_acronym_map = {v: k for k, v in
+                                  self.acronym_to_id_map.items()}
+
+        self.region_meshes_dict = MeshDictionary(self.root / "meshes")
 
     @property
     def reference(self):
@@ -38,14 +91,27 @@ class Atlas():
                 self._hemispheres = read_tiff(self.root / "hemispheres.tiff")
         return self._hemispheres
 
-    def get_point_hemisphere(self, point):
-        pass
+    def get_hemisphere_from_coords(self, coords):
+        return self.hemispheres[self.idx_from_coords(coords)]
 
-    def get_point_region(self, point):
-        pass
+    def get_region_id_from_coords(self, coords):
+        return self.annotated[self.idx_from_coords(coords)]
 
-    def get_region_mesh(self, region_id):
-        pass
+    def get_region_name_from_coords(self, coords):
+        region_id = self.get_region_id_from_coords(coords)
+
+        return self.acronym_map[region_id]
+
+    def get_mesh_from_id(self, region_id):
+        return self.region_meshes_dict[region_id]
+
+    def get_mesh_from_name(self, region_name):
+        region_id = self.acronym_map[region_name]
+        return self.get_mesh_from_id(region_id)
 
     def get_brain_mesh(self):
-        pass
+        return self.get_mesh_from_name("root")
+
+    @staticmethod
+    def idx_from_coords(coords):
+        return tuple([int(c) for c in coords])

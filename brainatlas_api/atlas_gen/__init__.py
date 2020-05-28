10759a9509c603c186e14daa4239fd799d5ae4c5
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-28 13:14:30 +0100

    fixedbuild

--- brainatlas_api/atlas_gen/__init__.py
+++ brainatlas_api/atlas_gen/__init__.py
@@ -1,4 +1,4 @@
-from atlas_gen.wrapup import wrapup_atlas_from_dir
-from atlas_gen.stacks import save_anatomy, save_annotation
+from brainatlas_api.atlas_gen.wrapup import wrapup_atlas_from_dir
+from brainatlas_api.atlas_gen.stacks import save_anatomy, save_annotation
 
-from atlas_gen import descriptors
+from brainatlas_api.atlas_gen import descriptors
--- brainatlas_api/atlas_gen/atlas_scripts/test_atlas.py
+++ brainatlas_api/atlas_gen/atlas_scripts/test_atlas.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import json
 import shutil
 
-from atlas_gen import (
+from brainatlas_api.atlas_gen import (
     save_anatomy,
     save_annotation,
     descriptors,

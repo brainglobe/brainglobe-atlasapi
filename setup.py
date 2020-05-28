a9154612df523cd4c3d0c4bc3cfff7490c5e0b9a
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-05-28 11:35:45 +0100

    update pypi descriptors

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requirements = ["tqdm", "numpy", "tifffile", "treelib"]
 setup(
     name="brainatlas-api",
     version="0.0.1b",
-    description="TBC",
+    description="A lightweight python module to interact with atlases for systems neuroscience",
     install_requires=requirements,
     extras_require={
         "dev": [
@@ -21,12 +21,22 @@ setup(
             "coverage",
         ]
     },
-    python_requires=">=3.6",
+    python_requires=">=3.6, <3.8",
     packages=find_namespace_packages(exclude=("docs", "tests*")),
     include_package_data=True,
     url="https://github.com/brainglobe/brainatlas-api",
     author="Luigi Petrucco, Federico Claudi, Adam Tyson",
     author_email="adam.tyson@ucl.ac.uk",
-    classifiers=["Development Status :: 3 - Alpha",],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System:: Microsoft:: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
     zip_safe=False,
 )

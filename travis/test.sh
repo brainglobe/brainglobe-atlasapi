684a97fc49e1d90084fb8fe78827bd1f273c7a97
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-05-31 12:40:58 +0200

    Added pytest install

--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,8 @@
-pip install -r requirements.txt
-pip install -e .
+pip install -r requirements.txt --no-cache-dir
+
 pip install black
+pip install pytest
+
 conda info -a
 black ./ -l 79 --target-version py37 --check
 pytest --cov=brainatlas_api

32908371d4eef9fef2e3e58f1381fa49f198cb13
FedeClaudi <federicoclaudi@protonmail.com>
2020-06-30 15:16:27 +0100

    increased coverage#

--- tests/test_update_atlas.py
+++ tests/test_update_atlas.py
@@ -4,6 +4,8 @@ from brainatlas_api import update
 def test_update():
     update.update_atlas("allen_mouse_25um")
 
+    update.update_atlas("allen_mouse_25um", force=True)
+
 
 def test_update_wrong_name():
     update.update_atlas("allen_madasadsdouse_25um")

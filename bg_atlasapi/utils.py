c46a5ed15c82c98b1f392d298cbfb327f10e9136
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-07-01 12:28:21 +0200

    Increased utils coverage

--- bg_atlasapi/utils.py
+++ bg_atlasapi/utils.py
@@ -15,11 +15,13 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 def check_internet_connection(
     url="http://www.google.com/", timeout=5, raise_error=True
 ):
-    """
-        Check that there is an internet connection
-
-        :param url: url to use for testing (Default value = 'http://www.google.com/')
-        :param timeout:  timeout to wait for [in seconds] (Default value = 5)
+    """Check that there is an internet connection
+    url : str
+        url to use for testing (Default value = 'http://www.google.com/')
+    timeout : int
+        timeout to wait for [in seconds] (Default value = 5).
+    raise_error : bool
+        if false, warning but no error.
     """
 
     try:
@@ -36,6 +38,16 @@ def check_internet_connection(
 
 
 def retrieve_over_http(url, output_file_path):
+    """Download file from remote location, with progress bar.
+
+    Parameters
+    ----------
+    url : str
+        Remote URL.
+    output_file_path : str or Path
+        Full file destination for download.
+
+    """
     CHUNK_SIZE = 4096
     response = requests.get(url, stream=True)
 
@@ -58,18 +70,22 @@ def retrieve_over_http(url, output_file_path):
 
 
 def conf_from_url(url):
+    """Read conf file from an URL.
+    Parameters
+    ----------
+    url : str
+        conf file url (in a repo, make sure the "raw" url is passed)
+
+    Returns
+    -------
+    conf object
+
+    """
     text = requests.get(url).text
     config = configparser.ConfigParser()
     config.read_string(text)
 
     return config
-
-
-def get_latest_atlases_version():
-    # TODO download version from online
-
-    versions = read_json("docs/atlases/latest_version.json")
-    return versions
 
 
 # --------------------------------- File I/O --------------------------------- #
---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bg_atlasapi import utils
+
+
+def test_http_check():
+    assert utils.check_internet_connection()
+    with pytest.raises(ConnectionError) as error:
+        utils.check_internet_connection(url="http://asd")
+    assert "No internet connection, try again" in str(error)
+
+    assert not utils.check_internet_connection(
+        url="http://asd", raise_error=False
+    )

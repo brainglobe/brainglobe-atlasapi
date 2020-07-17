a8391b0f039a4c10388cb5e91b900c856107b2c1
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-07-17 15:05:45 +0200

    Hemispheres now has as values 1&2 and it is instantiated finding frontal axis using bgspace description

--- bg_atlasapi/utils.py
+++ bg_atlasapi/utils.py
@@ -114,15 +114,3 @@ def read_tiff(path):
 #    """
 #    return [f.path for f in os.scandir(folderpath) if f.is_dir()]
 
-
-# ------------------------------- Data handling ------------------------------ #
-def make_hemispheres_stack(shape):
-    """ Make stack with hemispheres id. Assumes CCFv3 orientation.
-    0: left hemisphere, 1:right hemisphere.
-    :param shape: shape of the stack
-    :return:
-    """
-    stack = np.zeros(shape, dtype=np.uint8)
-    stack[(shape[0] // 2) :, :, :] = 1
-
-    return stack

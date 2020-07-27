1790818b08ba664950b906313e29aa22b9207973
Federico Claudi <federicoclaudi@protonmail.com>
2020-07-27 10:52:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="bg-atlasapi",
-    version="0.0.2rc0",
+    version="0.0.7rc0",
     description="A lightweight python module to interact with atlases for systems neuroscience",
     install_requires=requirements,
     extras_require={

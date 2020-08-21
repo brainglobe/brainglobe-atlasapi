c36fda511c8a544d823b2993a48ffe0e1edbaee2
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-08-21 14:17:58 +0200

    Bump version: 0.1.0 → 0.1.1

--- bg_atlasapi/__init__.py
+++ bg_atlasapi/__init__.py
@@ -1,5 +1,5 @@
 __author__ = """brainglobe"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from bg_atlasapi.bg_atlas import BrainGlobeAtlas
 from bg_atlasapi.list_atlases import show_atlases
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="bg-atlasapi",
-    version="0.1.0",
+    version="0.1.1",
     description="A lightweight python module to interact with atlases for systems neuroscience",
     install_requires=requirements,
     extras_require={

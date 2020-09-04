24c9e8afd7271aa3d1038240bbcb46fcd0d488a2
Luigi Petrucco <luigi.petrucco@gmail.com>
2020-09-04 10:38:34 +0200

    Trying to fix windows permission error when deleting folders

--- bg_atlasgen/main_script.py
+++ bg_atlasgen/main_script.py
@@ -4,29 +4,45 @@ from pathlib import Path
 import configparser
 import bg_atlasgen
 from importlib import import_module
-import shutil
 from bg_atlasapi.utils import atlas_name_from_repr, atlas_repr_from_name
+
+import errno, os, stat, shutil
+
+
+def handleRemoveReadonly(func, path, exc):
+    excvalue = exc[1]
+    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+        os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
+        func(path)
+    else:
+        raise
+
+
+def delete_folder(path):
+    shutil.rmtree(path, ignore_errors=False, onerror=handleRemoveReadonly)
+
 
 # Main dictionary specifying which atlases to generate
 # and with which resolutions:
 GENERATION_DICT = dict(mpin_zfish=[1],
-                       allen_mouse=[100],
+                       allen_mouse=[10, 25, 100],
                        example_mouse=[100],)
 
 
-cwd = Path.home() / "bg_auto"
+cwd = Path(r"D:\bg_auto")#Path.home() / "bg_auto"
 cwd.mkdir(exist_ok=True)
 
 
 if __name__ == "__main__":
     repo_path = cwd / "atlas_repo"
     if repo_path.exists():
-        # Clear folder:
-        shutil.rmtree(repo_path)
-    repo_path.mkdir()
+        repo = Repo(repo_path)
+        repo.git.pull()
+    else:
+        repo_path.mkdir(exist_ok=True)
 
-    print("Cloning atlases repo...")
-    repo = Repo.clone_from("https://gin.g-node.org/brainglobe/atlases", repo_path)
+        print("Cloning atlases repo...")
+        repo = Repo.clone_from("https://gin.g-node.org/brainglobe/atlases", repo_path)
     # us = input("GIN-GNode user: ")
     # pw = input("GIN-GNode password: ")
 
@@ -58,7 +74,7 @@ if __name__ == "__main__":
         script_version = module.__version__
 
         if bg_atlasgen_version > status["major_vers"] or \
-                (bg_atlasgen_version > status["major_vers"] and
+                (bg_atlasgen_version == status["major_vers"] and
                   script_version > status["minor_vers"]):
 
             # Loop over all resolutions:
@@ -66,7 +82,7 @@ if __name__ == "__main__":
                 print(f"Generating {name}, {resolution} um...")
 
                 # Make working directory for atlas generation:
-                temp_dir = cwd / "tempdir"
+                temp_dir = cwd / f"tempdir_{name}_{resolution}"
                 temp_dir.mkdir(exist_ok=True)
 
                 # Create and compress atlas:
@@ -74,7 +90,7 @@ if __name__ == "__main__":
 
                 # Move atlas to repo:
                 shutil.move(str(output_filename), repo_path)
-                shutil.rmtree(temp_dir)
+                # delete_folder(temp_dir)
 
                 # Update config file with new version:
                 k = atlas_name_from_repr(name, resolution)
@@ -83,15 +99,17 @@ if __name__ == "__main__":
                     conf.write(f)
 
                 # Update log for commit message:
-                commit_log += f"{output_filename}.name, "
+                commit_log += f"{output_filename.stem}, "
 
     # Commit and push:
     try:
         repo.git.add(".")
         repo.git.commit('-m', commit_log)
-        repo.git.push()
     except GitCommandError:
         pass
 
+    repo.git.push()
+
     # Clear folder:
-    shutil.rmtree(repo_path)
+    repo.close()
+    # delete_folder(repo_path)

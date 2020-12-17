10cc3a1ad8b875c8100828039428a33cf5744fa5
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-12-17 11:51:33 +0000

    move to GitHub actions

--- travis/lint.sh
+++
@@ -1,6 +0,0 @@
-
-pip install black
-pip install flake8
-
-flake8
-black ./ -l 79 --target-version py37 --check
--- travis/test.sh
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt --no-cache-dir
-
-pip install pytest
-pip install pytest-cov
-
-conda info -a
-pytest --runslow --cov=bg_atlasapi

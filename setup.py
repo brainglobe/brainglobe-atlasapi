179e26787c8f9882fe1d3af86be80b8d058939f8
Adam Tyson <code@adamltyson.com>
2022-07-29 14:03:19 +0100

    Align with other BG packages (#127)
    * Update tox config
    
    * Move reqs to setup.py
    
    * lint
    
    * update setup.py
    
    * remove changelog
    
    * Update badges

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
+from os import path
+
 from setuptools import find_namespace_packages, setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+this_directory = path.abspath(path.dirname(__file__))
 
-with open("requirements_dev.txt") as f:
-    requirements_dev = f.read().splitlines()
-
-with open("README.md") as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
+
+requirements = [
+    "bg-space >= 0.5.0",
+    "click",
+    "meshio",
+    "numpy",
+    "pandas",
+    "requests",
+    "rich >= 9.0.0",
+    "tifffile",
+    "treelib",
+]
+
 
 setup(
     name="bg-atlasapi",
@@ -17,23 +28,40 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
-    extras_require=dict(dev=requirements_dev),
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "gitpython",
+            "coverage>=5.0.3",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+            "check-manifest",
+        ]
+    },
     python_requires=">=3.6",
     entry_points={"console_scripts": ["brainglobe = bg_atlasapi.cli:bg_cli"]},
     packages=find_namespace_packages(exclude=("docs", "tests*")),
     include_package_data=True,
-    url="https://github.com/brainglobe/bg-atlasapi",
+    url="https://brainglobe.info/atlas-api",
+    project_urls={
+        "Source Code": "https://github.com/brainglobe/bg-atlasapi",
+        "Bug Tracker": "https://github.com/brainglobe/bg-atlasapi/issues",
+        "Documentation": "https://docs.brainglobe.info/bg-atlasapi",
+    },
     author="Luigi Petrucco, Federico Claudi, Adam Tyson",
-    author_email="adam.tyson@ucl.ac.uk",
+    author_email="code@adamltyson.com",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows :: Windows 10",
         "Operating System :: MacOS :: MacOS X",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
     ],

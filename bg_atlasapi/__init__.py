994fef4a1fe5ca1b802b70330460a976bbae6231
Adam Tyson <code@adamltyson.com>
2024-01-09 17:35:11 +0000

    Update versioning in __init__

--- bg_atlasapi/__init__.py
+++ bg_atlasapi/__init__.py
@@ -1,5 +1,13 @@
-__author__ = """brainglobe"""
-__version__ = "1.0.2"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("bg-atlasapi")
+    del version
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+__author__ = "BrainGlobe Developers"
 
 
 from bg_atlasapi.bg_atlas import BrainGlobeAtlas

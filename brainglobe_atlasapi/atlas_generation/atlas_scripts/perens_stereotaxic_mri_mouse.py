d479291564b9df3dbbed7a165dcb050cc80a060a
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2024-08-23 19:18:29 +0200

    fix formatting to pass checks

--- brainglobe_atlasapi/atlas_generation/atlas_scripts/perens_stereotaxic_mri_mouse.py
+++ brainglobe_atlasapi/atlas_generation/atlas_scripts/perens_stereotaxic_mri_mouse.py
@@ -161,7 +161,16 @@ def create_atlas(working_dir, resolution):
     destination_path = download_dir_path / "atlas_download.7z"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+        "Accept": (
+                "text/html,"
+                "application/xhtml+xml,"
+                "application/xml;q=0.9,"
+                "image/avif,"
+                "image/webp,"
+                "image/png,"
+                "image/svg+xml,"
+                "*/*;q=0.8"
+            ),   
         "Accept-Language": "en-GB,en;q=0.5",
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "DNT": "1",

3101c1027886f77f8927d0d2585f2701eaa9883e
alessandrofelder <alessandrofelder@users.noreply.github.com>
2024-11-04 09:58:01 +0000

    polish blackcap for Oldenburg visit

--- brainglobe_atlasapi/atlas_generation/atlas_scripts/blackcap.py
+++ brainglobe_atlasapi/atlas_generation/atlas_scripts/blackcap.py
@@ -1,18 +1,18 @@
 __version__ = "0"
 
+import csv
 import glob as glob
 import os
 import time
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 from brainglobe_utils.IO.image import load_nii
 from rich.progress import track
-from scipy import ndimage
-from skimage.filters.rank import median
-from skimage.morphology import ball
 
+from brainglobe_atlasapi.atlas_generation.annotation_utils import (
+    read_itk_labels,
+)
 from brainglobe_atlasapi.atlas_generation.mesh_utils import (
     Region,
     create_region_mesh,
@@ -34,16 +34,13 @@ def create_atlas(working_dir, resolution):
 
     # setup folder for downloading
 
-    atlas_path = Path(
-        "/media/ceph-neuroinformatics/neuroinformatics/atlas-forge/BlackCap/templates/template_sym_res-25um_n-18/for_atlas/"
-    )
+    atlas_path = Path.home() / "data/for_atlas/"
 
-    structures_file = atlas_path / "Label_description_BC74white_16_02_2024.txt"
+    structures_file = atlas_path / "sw/Label_definitions_1124_SW.txt"
     annotations_file = (
-        atlas_path / "sub-BC74_res-25um_labels_aligned-to-reference.nii.gz"
+        atlas_path / "sw/combined_annotations_sw_555v_smooth_cleaned.nii.gz"
     )
-    reference_file = atlas_path / "reference_res-25um_image.nii.gz"
-    reference_mask_file = atlas_path / "reference_res-25um_mask-4reg.nii.gz"
+    reference_file = atlas_path / "sw/reference_res-25_hemi-right_image.nii.gz"
     meshes_dir_path = Path.home() / "blackcap-meshes"
 
     try:
@@ -51,55 +48,39 @@ def create_atlas(working_dir, resolution):
     except FileExistsError:
         "mesh folder already exists"
 
-    # Read structures file
-    print("Reading structures file")
-    with open(
-        structures_file, mode="r", encoding="utf-8-sig"
-    ) as blackcap_file:
-        structure_data = pd.read_csv(
-            blackcap_file,
-            delimiter="\s+",
-            comment="#",
-            names=["IDX", "R", "G", "B", "A", "VIS", "MESH_VIS", "LABEL"],
-        )
+    print("Reading structures files")
+    hierarchy_path = atlas_path / "sw/hierarchy_1124_SW.csv"
+    small_to_main_map = {}
+    with open(hierarchy_path, mode="r") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header
+        for row in reader:
+            print(row)
+            if row[3]:
+                small_to_main_map[int(row[3])] = int(row[1])
 
-    # replace Clear Label (first row) with a root entry
-    structure_data["LABEL"].iloc[0] = "root"
-    structure_data["R"].iloc[0] = 255
-    structure_data["G"].iloc[0] = 255
-    structure_data["B"].iloc[0] = 255
-    structure_data["IDX"].iloc[0] = 999
+    structure_data_list = read_itk_labels(structures_file)
+    for structure in structure_data_list:
+        structure_id = structure["id"]
+        if structure_id in small_to_main_map.keys():
+            structure["structure_id_path"] = [
+                999,
+                small_to_main_map[structure_id],
+                structure_id,
+            ]
+        else:
+            structure["structure_id_path"] = [999, structure_id]
 
-    structure_data.rename(columns={"IDX": "id"}, inplace=True)
-    structure_data.rename(columns={"LABEL": "acronym"}, inplace=True)
-    structure_data["name"] = structure_data.apply(
-        lambda row: row["acronym"], axis=1
+    # append root structure
+    structure_data_list.append(
+        {
+            "id": 999,
+            "name": "root",
+            "acronym": "root",
+            "structure_id_path": [999],
+            "rgb_triplet": [255, 255, 255],
+        }
     )
-    structure_data["rgb_triplet"] = structure_data.apply(
-        lambda row: [str(row["R"]), str(row["G"]), str(row["B"])], axis=1
-    )
-    structure_data["structure_id_path"] = structure_data.apply(
-        lambda row: [row["id"]] if row["id"] == 999 else [999, row["id"]],
-        axis=1,
-    )
-
-    # drop columns we don't need
-    structure_data.drop(
-        columns=["A", "VIS", "MESH_VIS", "R", "G", "B"], inplace=True
-    )
-
-    structure_data_list = []
-    for _, row in structure_data.iterrows():
-        structure_data_list.append(
-            {
-                "id": row["id"],
-                "rgb_triplet": row["rgb_triplet"],
-                # "parent_structure_id": row["parent_structure_id"],
-                "name": row["name"],
-                "structure_id_path": row["structure_id_path"],
-                "acronym": row["acronym"],
-            }
-        )
 
     tree = get_structures_tree(structure_data_list)
     print(
@@ -113,41 +94,11 @@ def create_atlas(working_dir, resolution):
     annotated_volume = load_nii(annotations_file, as_array=True).astype(
         np.uint8
     )
-
-    # remove unconnected components
-    label_im, nb_labels = ndimage.label(
-        annotated_volume
-    )  # not to be confused with our labels
-    sizes = ndimage.sum(annotated_volume > 0, label_im, range(nb_labels + 1))
-    mask = sizes > 1000
-    annotated_volume *= mask[label_im]
-
-    # naive forcing symmetry
-    extent_LR = annotated_volume.shape[2]
-    half_image = extent_LR // 2
-
-    flipped_first_half = np.flip(
-        annotated_volume[:, :, 0:half_image], axis=2
-    ).copy()
-    flipped_second_half = np.flip(
-        annotated_volume[:, :, half_image - 1 : -1], axis=2
-    ).copy()
-
-    annotated_volume[:, :, 0:half_image] = np.minimum(
-        annotated_volume[:, :, 0:half_image], flipped_second_half
+    # Append a mirror image of the reference volume along axis 2
+    mirrored_volume = np.flip(annotated_volume, axis=2)
+    annotated_volume = np.concatenate(
+        (annotated_volume, mirrored_volume), axis=2
     )
-    annotated_volume[:, :, half_image - 1 : -1] = np.minimum(
-        annotated_volume[:, :, half_image - 1 : -1], flipped_first_half
-    )
-
-    # smooth annotations
-    annotated_volume = median(
-        annotated_volume, ball(3), mask=annotated_volume > 0
-    )
-
-    # keep only annotations in mask
-    brain_mask = load_nii(reference_mask_file, as_array=True).astype(np.uint16)
-    annotated_volume *= brain_mask
 
     # rescale reference volume into int16 range
     reference_volume = load_nii(reference_file, as_array=True)
@@ -157,11 +108,21 @@ def create_atlas(working_dir, resolution):
     dscale = (2**16 - 1) / drange
     reference_volume = (reference_volume - dmin) * dscale
     reference_volume = reference_volume.astype(np.uint16)
+    # Append a mirror image of the reference volume along axis 2
+    mirrored_volume = np.flip(reference_volume, axis=2)
+    reference_volume = np.concatenate(
+        (reference_volume, mirrored_volume), axis=2
+    )
+
+    has_label = annotated_volume > 0
+    reference_volume *= has_label
 
     # generate binary mask for mesh creation
     labels = np.unique(annotated_volume).astype(np.int_)
     for key, node in tree.nodes.items():
-        if key in labels:
+        if (
+            key in labels or key == 1
+        ):  # Pallium == 1 needs mesh but has no own voxels
             is_label = True
         else:
             is_label = False
@@ -170,7 +131,7 @@ def create_atlas(working_dir, resolution):
 
     # mesh creation
     closing_n_iters = 2
-    decimate_fraction = 0.3
+    decimate_fraction = 0.6  # higher = more triangles
     smooth = True
 
     start = time.time()
@@ -250,7 +211,7 @@ def create_atlas(working_dir, resolution):
 if __name__ == "__main__":
     res = 25, 25, 25
     home = str(Path.home())
-    bg_root_dir = Path.home() / "bg-atlasgen"
+    bg_root_dir = Path.home() / "brainglobe_workingdir"
     bg_root_dir.mkdir(exist_ok=True, parents=True)
 
     create_atlas(bg_root_dir, res)

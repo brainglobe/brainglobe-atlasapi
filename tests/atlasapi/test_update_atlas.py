0b348fe6aa34feb72c85b1e338d6d77d2ff0d461
Stella <30465823+stellaprins@users.noreply.github.com>
2025-03-14 15:41:26 +0000

    Add test_update_atlas_value_error

--- tests/atlasapi/test_update_atlas.py
+++ tests/atlasapi/test_update_atlas.py
@@ -13,3 +13,14 @@ def test_update_wrong_name():
     with pytest.raises(ValueError) as error:
         update_atlases.update_atlas("allen_madasadsdouse_25um")
     assert "is not a valid atlas name!" in str(error)
+
+
+def test_update_atlas_value_error(mocker):
+    """Test error on old atlas deletion failure."""
+    mocker.patch("shutil.rmtree")
+    expected_error = "Something went wrong while trying to delete the old"
+    " version of the atlas, aborting."
+    with pytest.raises(ValueError, match=expected_error):
+        update_atlases.update_atlas(
+            atlas_name="example_mouse_100um", force=True
+        )

d52cd1c876269c036e74f18273de8f65f1e0645b
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-03-25 18:39:48 +0000

    removes comments that are duplicating code in `tests/atlasapi/test_config_file.py`; and adds proper doctring for cli_modify_config and passed local pre-commits hooks #514

--- tests/atlasapi/test_config_file.py
+++ tests/atlasapi/test_config_file.py
@@ -28,34 +28,26 @@ def test_config_creation(conf_path):
 
 
 def test_config_edit(tmp_path):
-    # Create a CliRunner instance
     runner = CliRunner()
     result = runner.invoke(cli.bg_cli, ["config", "--show"])
 
-    # Assert the command ran successfully
     assert result.exit_code == 0
     assert result.output == config._print_config() + "\n"
 
-    # Read the current config
     config_pre = config.read_config()
     original_bg_folder = config_pre["default_dirs"]["brainglobe_dir"]
 
-    # Create a temporary directory for the new atlas directory
     new_atlas_dir = tmp_path / "new_brainglobe_dir"
     new_atlas_dir.mkdir()
 
-    # Replace the brainglobe_dir in the config with the temp directory
     config.write_config_value("brainglobe_dir", str(new_atlas_dir))
     config_post = config.read_config()
 
-    # Assert that the config was updated correctly
     assert config_post["default_dirs"]["brainglobe_dir"] == str(new_atlas_dir)
 
-    # Use new location to download the atlas
     atlas = bg_atlas.BrainGlobeAtlas(atlas_name="example_mouse_100um")
     assert atlas.root_dir.parent == new_atlas_dir
 
-    # Restore the original config value
     config.write_config_value("brainglobe_dir", original_bg_folder)
 
 
@@ -79,7 +71,7 @@ def test_cli_modify_config(
     monkeypatch, key, value_factory, expected_output, capsys, tmpdir
 ):
     def mock_write_config_value(k, v):
-        pass  # Mock function to bypass actual writing
+        pass
 
     monkeypatch.setattr(
         "brainglobe_atlasapi.config.write_config_value",

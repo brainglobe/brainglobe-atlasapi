dd6f08b5489117ab758b8536ee87d74c51b06a2d
Stella <30465823+stellaprins@users.noreply.github.com>
2025-03-26 16:13:01 +0000

    Add tests, parametrize test_check_struct_consistency
    added tests:
    - test_get_structure_children_no_children
    - test_get_structure_children_incomplete_region

--- tests/atlasgen/test_structures.py
+++ tests/atlasgen/test_structures.py
@@ -8,12 +8,32 @@ from brainglobe_atlasapi.atlas_generation.structures import (
 )
 
 
-def test_check_struct_consistency(structures):
-    """Test key mismatch error in structure consistency check."""
-    structures[0]["name_key_mismatch"] = structures[0].pop("name")
-    with pytest.raises(
-        AssertionError, match="Inconsistencies found for structure {'id': 101"
-    ):
+@pytest.mark.xfail  # TODO: remove after fixing value type mismatch bug
+@pytest.mark.parametrize(
+    "structure_transform, expected_error_message",
+    [
+        pytest.param(
+            lambda structures: structures[0].update(
+                {"key_mismatch": structures[0].pop("name")}
+            ),
+            "Inconsistencies found for structure {'id': 101",
+            id="key mismatch",
+        ),
+        pytest.param(
+            lambda structures: structures[0].update(
+                {"name": list(structures[0]["name"])}
+            ),
+            "Inconsistencies found for structure {'id': 101",
+            id="value type mismatch",
+        ),
+    ],
+)
+def test_check_struct_consistency(
+    structures, structure_transform, expected_error_message
+):
+    """Test structure consistency check with various modifications."""
+    structure_transform(structures)
+    with pytest.raises(AssertionError, match=expected_error_message):
         check_struct_consistency(structures)
 
 
@@ -47,6 +67,51 @@ def test_get_structure_children(
         structures=structures, region=region, use_tree=use_tree
     )
     assert children == expected_children
+
+
+@pytest.mark.xfail  # TODO: remove after get_structure_children bug fix
+@pytest.mark.parametrize(
+    ["use_tree"],
+    [
+        pytest.param(True, id="use_tree=True"),
+        pytest.param(False, id="use_tree=False"),
+    ],
+)
+def test_get_structure_children_no_children(structures, use_tree):
+    """Test retrieval of children from structures tree without children."""
+    region = structures[0]
+    structures = [region, structures[3]]
+    children = get_structure_children(
+        structures=structures, region=region, use_tree=use_tree
+    )
+    assert children is None
+
+
+@pytest.mark.parametrize(
+    ["dropped_key", "error"],
+    [
+        pytest.param("id", ValueError),
+        pytest.param("structure_id_path", ValueError),
+        pytest.param("name", None),
+    ],
+)
+def test_get_structure_children_incomplete_region(
+    dropped_key, error, structures
+):
+    """Error should be raised when region data is incomplete.
+
+    Error if 'id' or 'structure_id_path' is missing but not when other keys
+    (e.g. 'name') are missing."""
+    region = structures[0].copy()
+    region.pop(dropped_key)
+    if error is not None:
+        with pytest.raises(
+            ValueError, match="need both 'id' and 'structure_id_path'"
+        ):
+            get_structure_children(structures=structures, region=region)
+
+    else:
+        get_structure_children(structures=structures, region=region)
 
 
 @pytest.mark.parametrize(

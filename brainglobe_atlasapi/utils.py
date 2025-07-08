c72dbd8660189343b63359e9696fd5c09caa83d8
Alessandro Felder <alessandrofelder@users.noreply.github.com>
2025-07-08 15:47:45 +0200

    use unit consistently across conditionals in `atlas_name_from_repr` (#609)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- brainglobe_atlasapi/utils.py
+++ brainglobe_atlasapi/utils.py
@@ -86,36 +86,29 @@ def atlas_repr_from_name(name):
 
     atlas_name = "_".join(parts)
 
-    # For unspecified version:
+    # For specified version:
     if version_str:
         major_vers, minor_vers = version_str[1:].split(".")
-
-        # For versioned atlases, separate unit from resolution
-        unit = None
-        for res_unit in descriptors.RESOLUTION:
-            if resolution_str.endswith(res_unit):
-                unit = res_unit
-                resolution_str = resolution_str[: -len(res_unit)]
-                break
-
-        result = dict(
-            name=atlas_name,
-            major_vers=major_vers,
-            minor_vers=minor_vers,
-            resolution=resolution_str,
-        )
-        if unit:
-            result["unit"] = unit
-        return result
     else:
         major_vers, minor_vers = None, None
 
-    return dict(
+    # separate unit from resolution
+    unit = None
+    for res_unit in descriptors.RESOLUTION:
+        if resolution_str.endswith(res_unit):
+            unit = res_unit
+            resolution_str = resolution_str[: -len(res_unit)]
+            break
+
+    result = dict(
         name=atlas_name,
         major_vers=major_vers,
         minor_vers=minor_vers,
         resolution=resolution_str,
     )
+    if unit:
+        result["unit"] = unit
+    return result
 
 
 def atlas_name_from_repr(
@@ -123,7 +116,7 @@ def atlas_name_from_repr(
 ):
     """Generate atlas name given a description."""
     if major_vers is None and minor_vers is None:
-        return f"{name}_{resolution}"
+        return f"{name}_{resolution}{unit}"
     else:
         return f"{name}_{resolution}{unit}_v{major_vers}.{minor_vers}"
 

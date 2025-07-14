2166481303f998e1b94fe414e40376c7013c3e24
KAIXIANG SHUAI <108368488+Skxsmy@users.noreply.github.com>
2025-07-14 21:24:52 +0100

    applying modal filter to wingdisc annotation before packing (#599)
    * applying modal filter to wingdisc annotation before packing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove functionality for instant local test
    
    * document purpose of script
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix linting of multiline string
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Alessandro Felder <alessandrofelder@users.noreply.github.com>

---
+++ brainglobe_atlasapi/atlas_generation/atlas_scripts/pre_packaging/drosophila/filtering_wingdisc.py
@@ -0,0 +1,41 @@
+"""A one-off script to run a modal filter on some
+annotations from ITK snap inbetween manual
+improvement iterations."""
+
+from pathlib import Path
+
+import numpy as np
+from brainglobe_template_builder.io import (
+    save_as_asr_nii,
+)
+from brainglobe_utils.IO.image import load_nii
+from skimage.filters.rank import modal
+from skimage.morphology import square
+
+resources_path = Path("D:/UCL/Postgraduate_programme/templates/Version3")
+annotation_path = (
+    resources_path / "pouch_peripodial_hinge_notum_refined.nii.gz"
+)
+filtered_path = (
+    resources_path / "pouch_peripodial_hinge_notum_refined_filtered.nii.gz"
+)
+target_isotropic_resolution = 2
+if __name__ == "__main__":
+    # Load the annotation image
+    annotation_image = load_nii(annotation_path, as_array=True)
+    annotation_image = annotation_image.astype(np.uint16)
+
+    filtered_annotation_image = np.zeros_like(annotation_image)
+    footprint = square(5)  # Define a square footprint for the modal filter
+    for i in range(annotation_image.shape[0]):
+        # Apply the modal filter to each slice of the annotation image
+        filtered_stack = modal(annotation_image[i], footprint=footprint)
+        print(annotation_image[i].max())
+        filtered_annotation_image[i] = filtered_stack
+        print(filtered_stack.max())
+
+    # Save the filtered annotation image
+    vox_sizes = [
+        target_isotropic_resolution,
+    ] * 3
+    save_as_asr_nii(filtered_annotation_image, vox_sizes, filtered_path)

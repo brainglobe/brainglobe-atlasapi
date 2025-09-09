ecb91fb9474eceed72a07e42542fb7214042cfda
Harry Carey <harry.carey95@gmail.com>
2025-09-09 18:30:03 +0200

    fix formatting of docstrings

--- atlas_scripts/demba_allen_seg_dev_mouse.py
+++ atlas_scripts/demba_allen_seg_dev_mouse.py
@@ -39,8 +39,8 @@ resolution_to_modalities = {
 
 def download_resources(download_dir_path, atlas_file_url, atlas_name):
     """
-    Slight issue that the hash seems different each time. I think the
-    files are being zipped on the server each time we request and it's
+    Slight issue that the hash seems different each time. 
+    I think the files are being zipped on the server each time we request and it's
     changing the hash somehow (Maybe date and time is encoded in the
     file when zipped)
     """
@@ -140,7 +140,7 @@ def retrieve_additional_references(
     download_dir_path, age, resolution, modalities
 ):
     """
-    The additional references are MRI LSFM and Allen STPT.
+    retrieves the additional references which are MRI LSFM and Allen STPT.
     For the 10um Allen stpt is the main and only reference.
     This is because it is the only volume with 10um resolution.
     """
@@ -171,9 +171,7 @@ def retrieve_hemisphere_map():
 
 
 def retrieve_structure_information(download_path):
-    """
-    Use the allen ccf 2022 ontology via the allen ontology api
-    """
+    """Use the allen ccf 2022 ontology via the allen ontology api"""
     spacecache = ReferenceSpaceCache(
         manifest=download_path / "manifest.json",
         # downloaded files are stored relative to here

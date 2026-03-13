243285626f3ab989bddd43262122e4dcd9f05cbd
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-03-13 11:02:58 +0000

    Invert logic and remove potential invalid reference

--- brainglobe_atlasapi/update_atlases.py
+++ brainglobe_atlasapi/update_atlases.py
@@ -29,32 +29,27 @@ def update_atlas(atlas_name, force=False, fn_update=None):
         atlas_name=atlas_name, check_latest=False, fn_update=fn_update
     )
 
-    # Check if we need to update
-    if not force:
-        if atlas.check_latest_version(print_warning=False):
-            rprint(
-                f"[b][magenta2]brainglobe_atlasapi: {atlas.atlas_name} "
-                "is already updated "
-                f"(version: {_version_str_from_tuple(atlas.local_version)})"
-                "[/b]"
-            )
-            return
-    else:
-        if atlas.check_latest_version(print_warning=False):
-            # Delete atlas folder to force update
-            fld = (atlas.brainglobe_dir / atlas.local_full_name).parent
-            shutil.rmtree(fld)
+    is_latest_version = atlas.check_latest_version(print_warning=False)
+
+    if force and is_latest_version:
+        # Delete atlas folder to force update
+        fld = (atlas.brainglobe_dir / atlas.local_full_name).parent
+        shutil.rmtree(fld)
+
+    elif is_latest_version:
+        rprint(
+            f"[b][magenta2]brainglobe_atlasapi: {atlas.atlas_name} "
+            "is already updated "
+            f"(version: {_version_str_from_tuple(atlas.local_version)})"
+            "[/b]"
+        )
+
+        return
 
     rprint(
         "[b][magenta2]brainglobe_atlasapi: "
         f"updating {atlas.atlas_name}[/magenta2][/b]"
     )
-
-    if fld.exists():
-        raise ValueError(
-            "Something went wrong while trying to delete the old version "
-            "of the atlas, aborting."
-        )
 
     # Download again
     atlas.download()

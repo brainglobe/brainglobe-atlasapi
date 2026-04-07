d1901601c8676516c3ba8814d483ef31c41d2e93
kjungwoo5 <kjungwoo5@gmail.com>
2026-04-07 13:24:43 +0100

    Added more working code for processing downloaded files

--- atlas_scripts/hutchinson_ferret.py
+++ atlas_scripts/hutchinson_ferret.py
@@ -1,6 +1,20 @@
+import re
 from pathlib import Path
 
+import numpy as np
+import pooch
+import json
+import SimpleITK as sitk
+
+from pyarrow import csv
+
+from brainglobe_atlasapi import utils
+from brainglobe_utils.IO.image import load_any
+
 from brainglobe_atlasapi.atlas_generation.wrapup import wrapup_atlas_from_data
+from brainglobe_atlasapi.atlas_generation.mesh_utils import (
+    construct_meshes_from_annotation,
+)
 from brainglobe_atlasapi.utils import atlas_name_from_repr
 
 ### Metadata ###
@@ -28,7 +42,7 @@ ATLAS_LINK = "https://scalablebrainatlas.incf.org/templates/HSRetal17/source/evT
 
 # The orientation of the **original** atlas data, in BrainGlobe convention:
 # https://brainglobe.info/documentation/setting-up/image-definition.html#orientation
-ORIENTATION = "asr" #CHECK LATER
+ORIENTATION = "lpi" #CHECK LATER
 
 # The id of the highest level of the atlas. This is commonly called root or
 # brain. Include some information on what to do if your atlas is not
@@ -37,7 +51,7 @@ ROOT_ID = None #CHECK LATER
 
 # The resolution of your volume in microns. Details on how to format this
 # parameter for non isotropic datasets or datasets with multiple resolutions.
-RESOLUTION = None
+RESOLUTION = 500
 
 # --- Script toggles (no CLI args) ---
 # If True, do not re-download files that already exist on disk.
@@ -59,14 +73,72 @@ WHOLE_BRAIN_MESH_FNAME = "wholebrain.x3d"
 
 ATLAS_PACKAGER = "Jung Woo Kim"
 
+def hex_to_rgb(hex):
+    """Convert a hexadecimal color string to an RGB triplet.
+
+    Parameters
+    ----------
+    hex : str
+        The hexadecimal color string (e.g., "RRGGBB").
+
+    Returns
+    -------
+    list
+        A list of three integers representing the RGB color (0-255).
+    """
+    rgb = []
+    for i in (0, 2, 4):
+        intvalue = int(hex[i : i + 2], 16)
+        rgb.append(intvalue)
+
+    return rgb
 
 def download_resources():
-    """
-    Download the necessary resources for the atlas.
+    """Download the necessary resources for the atlas (with Pooch)."""
+    BG_ROOT_DIR.mkdir(exist_ok=True, parents=True)
+    DOWNLOAD_DIR_PATH.mkdir(exist_ok=True)
 
-    If possible, please use the Pooch library to retrieve any resources.
-    """
-    pass
+    template_path = DOWNLOAD_DIR_PATH / TEMPLATE_FNAME
+    annotation_path = DOWNLOAD_DIR_PATH / ANNOTATION_FNAME
+    labels_path = DOWNLOAD_DIR_PATH / LABELS_FNAME
+
+    needs_download = (
+        (not template_path.exists())
+        or (not annotation_path.exists())
+        or (not labels_path.exists())
+    )
+    if needs_download:
+        utils.check_internet_connection()
+
+    def should_fetch(path: Path) -> bool:
+        if not path.exists():
+            return True
+        return not SKIP_DOWNLOADS_IF_PRESENT
+
+    if should_fetch(template_path):
+        pooch.retrieve(
+            TEMPLATE_URL,
+            path=DOWNLOAD_DIR_PATH,
+            fname=TEMPLATE_FNAME,
+            known_hash="05f9a5cd5dc4c35d8f5eddf395fd3d79b6a40faeb99b08694adb59ebdb0fefbe",
+            progressbar=True,
+        )
+    if should_fetch(annotation_path):
+        pooch.retrieve(
+            ANNOTATION_URL,
+            path=DOWNLOAD_DIR_PATH,
+            fname=ANNOTATION_FNAME,
+            known_hash="f167a9aa8b67ec547e82609275530720153da6f37f2ae70128f54819af4b31f4",
+            progressbar=True,
+        )
+    if should_fetch(labels_path):
+        pooch.retrieve(
+            LABELS_URL,
+            path=DOWNLOAD_DIR_PATH,
+            fname=LABELS_FNAME,
+            known_hash="286f4cab8cd1a355574f221215d0075963c2f6a40c9b46c727804181637f4da1",
+            progressbar=True,
+        )
 
 
 def retrieve_reference_and_annotation():
@@ -80,8 +152,12 @@ def retrieve_reference_and_annotation():
     tuple[numpy.ndarray, numpy.ndarray]
         A tuple containing the reference volume and the annotation volume.
     """
-    reference = None
-    annotation = None
+    template_path = DOWNLOAD_DIR_PATH / TEMPLATE_FNAME
+    annotation_path = DOWNLOAD_DIR_PATH / ANNOTATION_FNAME
+    
+    reference = load_any(template_path, as_numpy=True)
+    annotation = load_any(annotation_path, as_numpy=True)
+    
     return reference, annotation
 
 
@@ -102,7 +178,7 @@ def retrieve_hemisphere_map():
     return None
 
 
-def retrieve_structure_information():
+def retrieve_structure_information(annotation_volume: np.ndarray):
     """
     Return a list of dictionaries with information about the atlas.
 
@@ -128,10 +204,50 @@ def retrieve_structure_information():
         A list of dictionaries, each containing information for a single
         atlas structure.
     """
-    return None
+    labels_path = DOWNLOAD_DIR_PATH / LABELS_FNAME
+    
+    # Filter structures to those actually present.
+    present_ids = set(map(int, np.unique(annotation_volume)))
+    
+    structures_by_id: dict[int, dict] = {
+        ROOT_ID: {
+            "id": ROOT_ID,
+            "name": "root",
+            "acronym": "root",
+            "structure_id_path": [ROOT_ID],
+            "rgb_triplet": [255, 255, 255],
+        }
+    }
+    
+    with open(labels_path) as f:
+        labels_data = json.load(f)
+
+    for label in labels_data:
+        if label[0] not in present_ids:
+            continue
+        if label[0] == 0 or label[2] == "Clear Label":
+            continue
+        id = label[0]
+        hex_colour = label[1]
+        acronym = label[2]
+        name = label[3]
+        rgb_colour = hex_to_rgb(hex_colour)
+        if id not in structures_by_id:
+            structures_by_id[id] = {
+                "id": id,
+                "name": name,
+                "acronym": acronym,
+                "structure_id_path": [ROOT_ID, id],
+                "rgb_triplet": rgb_colour,
+            }
+    
+    # Sort structures by depth of hierarchy, then ID. 
+    structures = list(structures_by_id.values())
+    structures.sort(key=lambda s: (len(s["structure_id_path"]), s["id"]))
+    return structures
 
 
-def retrieve_or_construct_meshes():
+def retrieve_or_construct_meshes(annotated_volume, structures, working_dir):
     """
     Return a dictionary mapping structure IDs to paths of mesh files.
 
@@ -144,7 +260,18 @@ def retrieve_or_construct_meshes():
         A dictionary where keys are structure IDs and values are paths to the
         corresponding mesh files.
     """
-    meshes_dict = {}
+    # Construct meshes from the annotation volume.
+    # Requires atlas generation extras: vedo + PyMCubes.
+    meshes_dict = construct_meshes_from_annotation(
+        save_path=Path(working_dir),
+        volume=annotated_volume,
+        structures_list=structures,
+        closing_n_iters=2,
+        decimate_fraction=0.2,
+        smooth=False,
+        parallel=True,
+        verbosity=0,
+    )
     return meshes_dict
 
 
@@ -161,6 +288,8 @@ def retrieve_additional_references():
     dict
         A dictionary mapping reference image names to their image stack data.
     """
+    
+    # ADD DEC AND OTHER IMAGES AVAILABLE ON THE WEBSITE
     additional_references = {}
     return additional_references
 
@@ -186,8 +315,12 @@ if __name__ == "__main__":
     reference_volume, annotated_volume = retrieve_reference_and_annotation()
     additional_references = retrieve_additional_references()
     hemispheres_stack = retrieve_hemisphere_map()
-    structures = retrieve_structure_information()
-    meshes_dict = retrieve_or_construct_meshes()
+    structures = retrieve_structure_information(annotated_volume)
+    meshes_dict = retrieve_or_construct_meshes(
+        annotated_volume=annotated_volume,
+        structures=structures,
+        working_dir=bg_root_dir,
+    )
 
     output_filename = wrapup_atlas_from_data(
         atlas_name=ATLAS_NAME,

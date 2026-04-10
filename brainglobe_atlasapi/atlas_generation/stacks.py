9878e48bb5b6f2fee8a229c70e0db6ff39a88673
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-04-10 13:41:52 +0100

    Update stacks functions for wrapup

--- brainglobe_atlasapi/atlas_generation/stacks.py
+++ brainglobe_atlasapi/atlas_generation/stacks.py
@@ -2,9 +2,46 @@
 atlas generation.
 """
 
+from pathlib import Path
+from typing import Dict, List
+
+import numpy.typing as npt
 import tifffile
+import zarr
+from ome_zarr.io import parse_url
+from ome_zarr.writer import write_multiscale
 
 from brainglobe_atlasapi import descriptors
+
+BG_OME_ZARR_AXES = [
+    {
+        "name": "z",
+        "type": "space",
+        "unit": "millimeter",
+        "orientation": {
+            "type": "anatomical",
+            "value": "anterior-to-posterior",
+        },
+    },
+    {
+        "name": "y",
+        "type": "space",
+        "unit": "millimeter",
+        "orientation": {
+            "type": "anatomical",
+            "value": "superior-to-inferior",
+        },
+    },
+    {
+        "name": "x",
+        "type": "space",
+        "unit": "millimeter",
+        "orientation": {
+            "type": "anatomical",
+            "value": "right-to-left",
+        },
+    },
+]
 
 
 def write_stack(stack, filename):
@@ -18,6 +55,40 @@ def write_stack(stack, filename):
         The path and filename where the stack will be saved.
     """
     tifffile.imwrite(str(filename), stack)
+
+
+def write_multiscale_ome_zarr(
+    images: List[npt.NDArray],
+    output_path: Path,
+    transformations: List[List[dict]],
+    axes: List[dict] = BG_OME_ZARR_AXES,
+):
+    """Write a multiscale image pyramid to an OME-Zarr file.
+
+
+    Parameters
+    ----------
+    images : list of np.ndarray
+        A list of image stacks representing different levels of the pyramid.
+    output_path : Path
+        The path where the OME-Zarr file will be saved.
+    transformations : list of lists of dicts
+        A list of transformations to be saved alongside the images.
+    axes : list of dicts, optional
+        A list of axis descriptors to be included in the OME-Zarr metadata.
+        If not provided, defaults to BG_OME_ZARR_AXES.
+    """
+    zarr_loc = parse_url(output_path, mode="w")
+    assert zarr_loc is not None
+    store = zarr_loc.store
+    root = zarr.group(store=store)
+
+    write_multiscale(
+        pyramid=images,
+        group=root,
+        axes=axes,
+        coordinate_transformations=transformations,
+    )
 
 
 def save_reference(stack, output_dir):
@@ -37,26 +108,39 @@ def save_reference(stack, output_dir):
     write_stack(stack, output_dir / descriptors.REFERENCE_FILENAME)
 
 
-def save_secondary_reference(stack, name, output_dir):
-    """Save a secondary reference image stack with a given name.
+def save_template(
+    stack: npt.NDArray, output_dir: Path, transformations: List[List[Dict]]
+):
+    """Save the template image stack along with its transformations.
 
     Ensures the stack is of the correct data type before saving.
 
     Parameters
     ----------
     stack : np.ndarray
-        The secondary reference image stack.
-    name : str
-        The base name for the output file (e.g., "my_secondary_reference").
+        The template image stack.
     output_dir : Path
-        The directory where the secondary reference image will be saved.
+        The directory where the template image will be saved.
+    transformations : list of lists of dicts
+        A list of transformations to be saved alongside the template.
     """
     if stack.dtype != descriptors.REFERENCE_DTYPE:
         stack = stack.astype(descriptors.REFERENCE_DTYPE)
-    write_stack(stack, output_dir / f"{name}.tiff")
+
+    assert (
+        len(transformations) == 1
+    ), "Currently only one resolution level is supported for the template."
+
+    write_multiscale_ome_zarr(
+        images=[stack],
+        output_path=(output_dir / descriptors.V2_TEMPLATE_NAME),
+        transformations=transformations,
+    )
 
 
-def save_annotation(stack, output_dir):
+def save_annotation(
+    stack: npt.NDArray, output_dir: Path, transformations: List[List[Dict]]
+):
     """Save the annotation image stack.
 
     Ensures the stack is of the correct data type before saving.
@@ -67,13 +151,26 @@ def save_annotation(stack, output_dir):
         The annotation image stack.
     output_dir : Path
         The directory where the annotation image will be saved.
+    transformations : list of lists of dicts
+        A list of transformations to be saved alongside the annotation.
     """
     if stack.dtype != descriptors.ANNOTATION_DTYPE:
         stack = stack.astype(descriptors.ANNOTATION_DTYPE)
-    write_stack(stack, output_dir / descriptors.ANNOTATION_FILENAME)
+
+    assert (
+        len(transformations) == 1
+    ), "Currently only one resolution level is supported for the annotation."
+
+    write_multiscale_ome_zarr(
+        images=[stack],
+        output_path=output_dir / (descriptors.V2_ANNOTATION_NAME),
+        transformations=transformations,
+    )
 
 
-def save_hemispheres(stack, output_dir):
+def save_hemispheres(
+    stack: npt.NDArray, output_dir: Path, transformations: List[List[Dict]]
+):
     """Save the hemispheres image stack.
 
     Ensures the stack is of the correct data type before saving.
@@ -84,7 +181,18 @@ def save_hemispheres(stack, output_dir):
         The hemispheres image stack.
     output_dir : Path
         The directory where the hemispheres image will be saved.
+    transformations : list of lists of dicts
+        A list of transformations to be saved alongside the hemispheres.
     """
     if stack.dtype != descriptors.HEMISPHERES_DTYPE:
         stack = stack.astype(descriptors.HEMISPHERES_DTYPE)
-    write_stack(stack, output_dir / descriptors.HEMISPHERES_FILENAME)
+
+    assert (
+        len(transformations) == 1
+    ), "Currently only one resolution level is supported for the hemispheres."
+
+    write_multiscale_ome_zarr(
+        images=[stack],
+        output_path=output_dir / descriptors.V2_HEMISPHERES_NAME,
+        transformations=transformations,
+    )

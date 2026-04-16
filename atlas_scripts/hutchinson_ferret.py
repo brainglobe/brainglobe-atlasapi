a07a4d512800b70efccdbedce199ac88657a27ed
kjungwoo5 <kjungwoo5@gmail.com>
2026-04-16 16:12:12 +0100

    Added atlas packager name

--- atlas_scripts/hutchinson_ferret.py
+++ atlas_scripts/hutchinson_ferret.py
@@ -339,4 +339,5 @@ if __name__ == "__main__":
         compress=True,
         scale_meshes=True,
         additional_references=additional_references,
+        atlas_packager=ATLAS_PACKAGER,
     )

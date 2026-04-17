40e0ee64b489a9a7e0cb47e96af0e12a8ad99442
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-04-17 17:27:20 +0100

    WIP multi-resolution wrap up

--- brainglobe_atlasapi/atlas_generation/wrapup.py
+++ brainglobe_atlasapi/atlas_generation/wrapup.py
@@ -3,7 +3,7 @@
 import json
 import shutil
 from pathlib import Path
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, Dict, List, Optional, Tuple, Union
 
 import brainglobe_space as bgs
 import meshio as mio
@@ -221,7 +221,7 @@ def _make_component_metadata(
 
 
 def _save_if_not_exists(
-    stack: npt.NDArray,
+    stacks: List[npt.NDArray],
     dest_dir: Path,
     label: str,
     transformations: List[List[dict]],
@@ -230,7 +230,7 @@ def _save_if_not_exists(
     if dest_dir.exists():
         print(f"{label} directory already exists, skipping: {dest_dir}")
     else:
-        save_fn(stack, dest_dir, transformations)
+        save_fn(stacks, dest_dir, transformations)
 
 
 def _check_validations(validation_results: dict) -> None:
@@ -258,19 +258,21 @@ def _check_validations(validation_results: dict) -> None:
 
 
 def wrapup_atlas_from_data(
-    atlas_name,
-    atlas_minor_version,
-    citation,
-    atlas_link,
-    species,
-    resolution,
-    orientation,
-    root_id,
-    reference_stack,
-    annotation_stack,
-    structures_list,
-    meshes_dict,
-    working_dir,
+    atlas_name: str,
+    atlas_minor_version: Union[int, str],
+    citation: str,
+    atlas_link: str,
+    species: str,
+    resolution: Tuple[int | float] | List[Tuple[int | float]],
+    orientation: str,
+    root_id: int,
+    reference_stack: str | Path | npt.NDArray | List[str | Path | npt.NDArray],
+    annotation_stack: (
+        str | Path | npt.NDArray | List[str | Path | npt.NDArray]
+    ),
+    structures_list: List[Dict],
+    meshes_dict: Dict[int | str, str | Path],
+    working_dir: str | Path,
     atlas_packager=None,
     hemispheres_stack=None,
     template_info: Optional[Tuple[str, str, bool]] = None,
@@ -305,15 +307,19 @@ def wrapup_atlas_from_data(
         (tuple describing origin for BGSpace).
     root_id :
         Id of the root element of the atlas.
-    reference_stack : str or Path or numpy array
+    reference_stack : str | Path | npt.NDArray | List[str | Path | npt.NDArray]
         Reference stack for the atlas.
         If str or Path, will be read with tifffile.
-    annotation_stack : str or Path or numpy array
+        If list, should be list of stacks for each scale, ordered from highest
+        to lowest resolution.
+    annotation_stack : str | Path | npt.NDArray | List[str | Path | npt.NDArray]
         Annotation stack for the atlas.
         If str or Path, will be read with tifffile.
-    structures_list : list of dict
+        If list, should be list of stacks for each scale, ordered from highest
+        to lowest resolution.
+    structures_list : List[Dict]
         List of valid dictionary for structures.
-    meshes_dict : dict
+    meshes_dict : Dict[int | str, str | Path]
         dict of meshio-compatible mesh file paths in the form
         {struct_id: meshpath}
     working_dir : str or Path obj
@@ -321,19 +327,21 @@ def wrapup_atlas_from_data(
     atlas_packager : str or None
         Credit for those responsible for converting the atlas
         into the BrainGlobe format.
-    hemispheres_stack : str or Path or numpy array, optional
+    hemispheres_stack : str | Path | npt.NDArray | List[str | Path | npt.NDArray] | None, optional
         Hemisphere stack for the atlas.
         If str or Path, will be read with tifffile.
+        If list, should be list of stacks for each scale, ordered from highest
+        to lowest resolution.
         If none is provided, atlas is assumed to be symmetric.
     scale_meshes: bool, optional
         (Default values = False).
         If True the meshes points are scaled by the resolution
         to ensure that they are specified in microns,
         regardless of the atlas resolution.
-    resolution_mapping: list, optional
+    resolution_mapping: List[int], optional
         a list of three mapping the target space axes to the source axes
         only needed for mesh scaling of anisotropic atlases
-    additional_references: List[Tuple[Dict, npt.NDArray]], optional
+    additional_references: List[Tuple[Dict, str | Path | npt.NDArray | List[str | Path | npt.NDArray]]], optional
         (Default value = empty list).
         List of tuples containing metadata and arrays for secondary templates.
     additional_metadata: dict, optional
@@ -343,18 +351,10 @@ def wrapup_atlas_from_data(
         (Default value = False).
         If True, will overwrite existing atlas directory.
         If False and atlas directory exists, raises FileExistsError.
-    """
+    """  # noqa: E501
     working_dir = Path(working_dir) / "brainglobe-atlasapi"
     atlas_version = f"{ATLAS_VERSION}.{atlas_minor_version}"
     atlas_version_underscore = atlas_version.replace(".", "_")
-
-    atlas_name_with_res = f"{atlas_name}_{resolution[0]}um"
-
-    atlas_location = (
-        f"/{descriptors.V2_ATLAS_ROOTDIR}/"
-        f"{atlas_name_with_res}/{atlas_version_underscore}"
-    )
-    atlas_dir = Path(working_dir) / atlas_location.strip("/")
 
     annotation_name, annotation_version, skip_annotation_saving = (
         _resolve_component_info(
@@ -433,37 +433,29 @@ def wrapup_atlas_from_data(
         for res_tuple in resolution_standard
     ]
 
-    if atlas_dir.exists():
-        if overwrite:
-            print(f"Atlas directory already exists, overwriting: {atlas_dir}")
-            shutil.rmtree(atlas_dir)
-        else:
-            raise FileExistsError(
-                f"Atlas output already exists at {atlas_dir}. "
-                "Try setting overwrite=True"
-            )
-
-    # exist_ok would be more permissive but error-prone here as there might
-    # be old files
-    atlas_dir.mkdir(parents=True)
-
     # If no hemisphere file is given, assume the atlas is symmetric:
     symmetric = hemispheres_stack is None
 
     if isinstance(annotation_stack, str) or isinstance(annotation_stack, Path):
-        annotation_stack = tifffile.imread(annotation_stack)
+        annotation_stack = [tifffile.imread(annotation_stack)]
+    elif isinstance(annotation_stack, npt.NDArray):
+        annotation_stack = [annotation_stack]
 
     if isinstance(reference_stack, str) or isinstance(reference_stack, Path):
-        reference_stack = tifffile.imread(reference_stack)
+        reference_stack = [tifffile.imread(reference_stack)]
+    elif isinstance(reference_stack, npt.NDArray):
+        reference_stack = [reference_stack]
 
     structures_list = filter_structures_not_present_in_annotation(
-        structures_list, annotation_stack
+        structures_list, annotation_stack[0]
     )
 
     # Instantiate BGSpace obj, using original stack size in um as meshes
     # are un um:
-    original_shape = reference_stack.shape
-    volume_shape = tuple(res * s for res, s in zip(resolution, original_shape))
+    original_shape = reference_stack[0].shape
+    volume_shape = tuple(
+        res * s for res, s in zip(resolution_standard[0], original_shape)
+    )
     space_convention = bgs.AnatomicalSpace(orientation, shape=volume_shape)
 
     # Check consistency of structures:
@@ -472,10 +464,15 @@ def wrapup_atlas_from_data(
     # Write template:
     if not skip_template_saving:
         # Reorient stacks if required:
-        reference_stack = space_convention.map_stack_to(
-            descriptors.ATLAS_ORIENTATION, reference_stack, copy=False
-        )
-        shape = reference_stack.shape
+        reference_stack = [
+            space_convention.map_stack_to(
+                descriptors.ATLAS_ORIENTATION, stack, copy=False
+            )
+            for stack in reference_stack
+        ]
+
+        shape = reference_stack[0].shape
+        shapes = [stack.shape for stack in reference_stack]
         dest_dir = working_dir / template_metadata["location"].lstrip("/")
 
         _save_if_not_exists(
@@ -491,16 +488,20 @@ def wrapup_atlas_from_data(
             / template_metadata["location"].lstrip("/")
             / descriptors.V2_TEMPLATE_NAME
         )
-        shape = multiscale.images[0].data.shape
+
+        shapes = [image.data.shape for image in multiscale.images]
 
     # Write annotation:
     if not skip_annotation_saving:
         # Reorient stacks if required:
-        annotation_stack = space_convention.map_stack_to(
-            descriptors.ATLAS_ORIENTATION, annotation_stack, copy=False
-        )
-        shape = annotation_stack.shape
+        annotation_stack = [
+            space_convention.map_stack_to(
+                descriptors.ATLAS_ORIENTATION, stack, copy=False
+            )
+            for stack in annotation_stack
+        ]
 
+        shapes = [stack.shape for stack in annotation_stack]
         dest_dir = working_dir / annotation_metadata["location"].lstrip("/")
 
         _save_if_not_exists(
@@ -513,12 +514,15 @@ def wrapup_atlas_from_data(
 
         if hemispheres_stack is None:
             # initialize empty stack:
-            hemispheres_stack = np.full(shape, 2, dtype=np.uint8)
+            hemispheres_stack = [
+                np.full(shape, 2, dtype=np.uint8) for shape in shapes
+            ]
 
             # Fill out with 2s the right hemisphere:
-            slices = [slice(None) for _ in range(3)]
-            slices[2] = slice(round(hemispheres_stack.shape[2] / 2), None)
-            hemispheres_stack[tuple(slices)] = 1
+            slices = [slice(None) for _ in range(3)] * len(annotation_stack)
+            for stack in hemispheres_stack:
+                slices[2] = slice(round(stack.shape[2] / 2), None)
+                stack[tuple(slices)] = 1
 
             dest_dir = (
                 working_dir
@@ -558,14 +562,14 @@ def wrapup_atlas_from_data(
                     # in target space (typically ASR)
                     if not resolution_mapping:
                         # isotropic case, so don't need to re-map resolution
-                        mesh.points *= resolution
+                        mesh.points *= resolution_standard[0]
                     else:
                         # resolution needs to be transformed back
                         # to original space in anisotropic case
                         original_resolution = (
-                            resolution[resolution_mapping[0]],
-                            resolution[resolution_mapping[1]],
-                            resolution[resolution_mapping[2]],
+                            resolution_standard[0][resolution_mapping[0]],
+                            resolution_standard[0][resolution_mapping[1]],
+                            resolution_standard[0][resolution_mapping[2]],
                         )
                         mesh.points *= original_resolution
 
@@ -584,10 +588,20 @@ def wrapup_atlas_from_data(
 
     additional_references_metadata = []
     for ref_tuple in additional_references:
-        ref_metadata, stack = ref_tuple
-        stack = space_convention.map_stack_to(
-            descriptors.ATLAS_ORIENTATION, stack, copy=False
-        )
+        ref_metadata, additional_stack = ref_tuple
+        if isinstance(additional_stack, str) or isinstance(
+            additional_stack, Path
+        ):
+            additional_stack = [tifffile.imread(additional_stack)]
+        elif isinstance(additional_stack, npt.NDArray):
+            additional_stack = [additional_stack]
+
+        additional_stack = [
+            space_convention.map_stack_to(
+                descriptors.ATLAS_ORIENTATION, stack, copy=False
+            )
+            for stack in additional_stack
+        ]
         if isinstance(ref_metadata, str):
             ref_name = f"{atlas_name}-{ref_metadata}-template"
             ref_metadata = _make_component_metadata(
@@ -599,7 +613,7 @@ def wrapup_atlas_from_data(
         dest_dir = working_dir / ref_metadata["location"].lstrip("/")
 
         _save_if_not_exists(
-            stack,
+            additional_stack,
             dest_dir,
             ref_metadata["name"],
             transformations,
@@ -671,50 +685,77 @@ def wrapup_atlas_from_data(
             with open(coordinate_space_metadata_path, "w") as f:
                 json.dump(coordinate_space_metadata, f, indent=4)
 
-    # Finalize metadata dictionary:
-    metadata_dict = generate_metadata_dict(
-        name=atlas_name,
-        location=atlas_location,
-        citation=citation,
-        atlas_link=atlas_link,
-        species=species,
-        symmetric=symmetric,
-        resolution=resolution,  # We expect input to be asr
-        orientation=descriptors.ATLAS_ORIENTATION,  # Pass orientation "asr"
-        version=f"{ATLAS_VERSION}.{atlas_minor_version}",
-        shape=shape,
-        additional_references=additional_references_metadata,
-        atlas_packager=atlas_packager,
-        coordinate_space_metadata=coordinate_space_metadata,
-        terminology_metadata=terminology_metadata,
-        annotation_set_metadata=annotation_metadata,
-        template_metadata=template_metadata,
-    )
+    for resolution, shape in zip(resolution_standard, shapes):
+        # Finalize metadata dictionary:
+        atlas_name_with_res = f"{atlas_name}_{resolution[0]}um"
 
-    with open(atlas_dir / "manifest.json", "w") as f:
-        json.dump(metadata_dict, f, indent=4)
+        atlas_location = (
+            f"/{descriptors.V2_ATLAS_ROOTDIR}/"
+            f"{atlas_name_with_res}/{atlas_version_underscore}"
+        )
+        atlas_dir = Path(working_dir) / atlas_location.strip("/")
 
-    atlas_name_for_validation = atlas_name_from_repr(atlas_name, resolution[0])
+        if atlas_dir.exists():
+            if overwrite:
+                print(
+                    f"Atlas directory already exists, overwriting: {atlas_dir}"
+                )
+                shutil.rmtree(atlas_dir)
+            else:
+                raise FileExistsError(
+                    f"Atlas output already exists at {atlas_dir}. "
+                    "Try setting overwrite=True"
+                )
 
-    # creating BrainGlobe object from local folder (working_dir)
-    atlas_to_validate = BrainGlobeAtlas(
-        atlas_name=atlas_name_for_validation,
-        brainglobe_dir=working_dir.parent,
-        check_latest=False,
-    )
+        # exist_ok would be more permissive but error-prone here as there might
+        # be old files
+        atlas_dir.mkdir(parents=True)
 
-    # Run validation functions
-    print(f"Running atlas validation on {atlas_location}")
+        metadata_dict = generate_metadata_dict(
+            name=atlas_name_with_res,
+            location=atlas_location,
+            citation=citation,
+            atlas_link=atlas_link,
+            species=species,
+            symmetric=symmetric,
+            resolution=resolution,  # We expect input to be asr
+            orientation=descriptors.ATLAS_ORIENTATION,
+            version=f"{ATLAS_VERSION}.{atlas_minor_version}",
+            shape=shape,
+            additional_references=additional_references_metadata,
+            atlas_packager=atlas_packager,
+            coordinate_space_metadata=coordinate_space_metadata,
+            terminology_metadata=terminology_metadata,
+            annotation_set_metadata=annotation_metadata,
+            template_metadata=template_metadata,
+        )
 
-    validation_results = {}
+        with open(atlas_dir / "manifest.json", "w") as f:
+            json.dump(metadata_dict, f, indent=4)
 
-    for func in get_all_validation_functions():
-        try:
-            func(atlas_to_validate)
-            validation_results[func.__name__] = "Pass"
-        except AssertionError as e:
-            validation_results[func.__name__] = f"Fail: {str(e)}"
+        atlas_name_for_validation = atlas_name_from_repr(
+            atlas_name, resolution[0]
+        )
 
-    _check_validations(validation_results)
+        # creating BrainGlobe object from local folder (working_dir)
+        atlas_to_validate = BrainGlobeAtlas(
+            atlas_name=atlas_name_for_validation,
+            brainglobe_dir=working_dir.parent,
+            check_latest=False,
+        )
+
+        # Run validation functions
+        print(f"Running atlas validation on {atlas_location}")
+
+        validation_results = {}
+
+        for func in get_all_validation_functions():
+            try:
+                func(atlas_to_validate)
+                validation_results[func.__name__] = "Pass"
+            except AssertionError as e:
+                validation_results[func.__name__] = f"Fail: {str(e)}"
+
+        _check_validations(validation_results)
 
     return atlas_dir

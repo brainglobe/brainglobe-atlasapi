647319b76f1ed7c14bb146b06afa1e7d36e0be59
IgorTatarnikov <igor.tatarnikov@gmail.com>
2026-04-30 17:46:23 +0100

    Add functions for generating file stubs for each component

--- brainglobe_atlasapi/descriptors.py
+++ brainglobe_atlasapi/descriptors.py
@@ -69,3 +69,142 @@ HEMISPHERES_DTYPE = np.uint8
 # Standard orientation origin: Anterior, Superior, Right
 # (using brainglobe-space definition)
 ATLAS_ORIENTATION = "asr"
+
+
+def format_component_stub(
+    component_name: str,
+    component_version: str,
+    component_root_dir: str,
+    component_file_name: str,
+) -> str:
+    """
+    Format the component stub for a given component name, version,
+    root directory and file name.
+
+    Parameters
+    ----------
+    component_name : str
+        The name of the component (e.g., allen-mouse-adult-stpt-template).
+    component_version : str
+        The version of the component.
+    component_root_dir : str
+        The root directory of the component.
+    component_file_name : str
+        The name of the component file.
+
+    Returns
+    -------
+    str
+        The formatted component stub.
+    """
+    component_version = component_version.replace(".", "_")
+    stub = (
+        f"{component_root_dir}/{component_name}/"
+        f"{component_version}/{component_file_name}"
+    )
+    return stub
+
+
+def format_template_stub(template_name: str, version: str) -> str:
+    """
+    Format the template stub for a given template name and version.
+
+    Parameters
+    ----------
+    template_name : str
+        The name of the template (e.g., allen-adult-mouse-stpt-template).
+    version : str
+        The version of the template.
+
+    Returns
+    -------
+    str
+        The formatted template stub.
+    """
+    return format_component_stub(
+        template_name, version, V2_TEMPLATE_ROOTDIR, V2_TEMPLATE_NAME
+    )
+
+
+def format_annotation_stub(annotation_name: str, version: str) -> str:
+    """
+    Format the annotation stub for a given annotation name and version.
+
+    Parameters
+    ----------
+    annotation_name : str
+        The name of the annotation (e.g., allen-adult-mouse-annotation).
+    version : str
+        The version of the annotation.
+
+    Returns
+    -------
+    str
+        The formatted annotation stub.
+    """
+    return format_component_stub(
+        annotation_name, version, V2_ANNOTATION_ROOTDIR, V2_ANNOTATION_NAME
+    )
+
+
+def format_hemispheres_stub(annotation_name: str, version: str) -> str:
+    """
+    Format the hemispheres stub for a given hemispheres name and version.
+
+    Parameters
+    ----------
+    annotation_name : str
+        The name of the annotation (e.g., allen-adult-mouse-annotation).
+    version : str
+        The version of the annotation.
+
+    Returns
+    -------
+    str
+        The formatted hemispheres stub.
+    """
+    return format_component_stub(
+        annotation_name, version, V2_ANNOTATION_ROOTDIR, V2_HEMISPHERES_NAME
+    )
+
+
+def format_terminology_stub(terminology_name: str, version: str) -> str:
+    """
+    Format the terminology stub for a given terminology name and version.
+
+    Parameters
+    ----------
+    terminology_name : str
+        The name of the terminology (e.g., allen-adult-mouse-terminology).
+    version : str
+        The version of the terminology.
+
+    Returns
+    -------
+    str
+        The formatted terminology stub.
+    """
+    return format_component_stub(
+        terminology_name, version, V2_TERMINOLOGY_ROOTDIR, V2_TERMINOLOGY_NAME
+    )
+
+
+def format_meshes_stub(annotation_name: str, version: str) -> str:
+    """
+    Format the meshes stub for a given annotation name and version.
+
+    Parameters
+    ----------
+    annotation_name : str
+        The name of the annotation (e.g., allen-adult-mouse-annotation).
+    version : str
+        The version of the annotation.
+
+    Returns
+    -------
+    str
+        The formatted meshes stub.
+    """
+    return format_component_stub(
+        annotation_name, version, V2_ANNOTATION_ROOTDIR, V2_MESHES_DIRECTORY
+    )

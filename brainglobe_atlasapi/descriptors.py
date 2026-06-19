32f3ba05b3758168d09e248eb94efe8ae0d88fe5
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-06-19 10:41:10 +0100

    Adds rc-2 to the http s3 link

--- brainglobe_atlasapi/descriptors.py
+++ brainglobe_atlasapi/descriptors.py
@@ -9,7 +9,9 @@ import numpy.typing as npt
 # Base url of the gin repository:
 remote_url_base = "https://gin.g-node.org/brainglobe/atlases/raw/master/{}"
 remote_url_s3 = "s3://brainglobe/atlas-rc2/{}"
-remote_url_s3_http = "https://brainglobe.s3.us-west-2.amazonaws.com/atlas/{}"
+remote_url_s3_http = (
+    "https://brainglobe.s3.us-west-2.amazonaws.com/atlas-rc2/{}"
+)
 
 # Major version of atlases used by current brainglobe-atlasapi release:
 ATLAS_MAJOR_V = 0

844a36098920a6c0cd9743f0a0be3aae045d7fde
Igor Tatarnikov <61896994+IgorTatarnikov@users.noreply.github.com>
2026-07-20 15:18:50 +0100

    Change mask caching strategy (#861)
    * Changes caching strategy to use the mesh index instead of the corner of the array
    
    * Adds test to make sure mask is cached from S3
    
    * Clarify comment

--- brainglobe_atlasapi/core.py
+++ brainglobe_atlasapi/core.py
@@ -706,13 +706,10 @@ class Atlas:
         dataset_path = multiscale.metadata.datasets[
             self._annotation_masks_pyramid_level
         ].path
-        # Zarr v3 stores chunk i of a (1,Z,Y,X) array at c/i/0/0/0
-        # Use the presence of the corner chunk as a proxy for
-        # existance, if not, download recursively the whole mask
-        chunk_path = (
-            masks_path / dataset_path / "c" / str(index) / "0" / "0" / "0"
-        )
-        if not chunk_path.exists():
+        # Check if the mask is cached locally. The presence of the
+        # directory for the specific index indicates that the mask is cached.
+        local_path = masks_path / dataset_path / "c" / str(index)
+        if not local_path.exists():
             annotation_location = self.metadata["annotation_set"]["location"][
                 1:
             ]
@@ -720,8 +717,6 @@ class Atlas:
                 f"{annotation_location}/{V3_ANNOTATION_MASKS_NAME}"
                 f"/{dataset_path}/c/{index}/"
             )
-            local_path = masks_path / dataset_path / "c" / str(index)
-            chunk_path.parent.mkdir(parents=True, exist_ok=True)
             try:
                 self.fs.get(
                     remote_path,

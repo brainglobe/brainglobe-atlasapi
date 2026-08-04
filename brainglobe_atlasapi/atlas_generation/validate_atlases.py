Fix centre calculation for atlas validation (#897)
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Igor Tatarnikov <61896994+IgorTatarnikov@users.noreply.github.com>

brainglobe_atlasapi/atlas_generation/validate_atlases.py
@@ -451,9 +451,14 @@ def validate_annotation_symmetry(atlas: BrainGlobeAtlas):
         If the annotation labels at the chosen symmetric points are different.
     """
     annotation = atlas.annotation
-    centre = np.array(annotation.shape) // 2
+
+    annotation_shape = np.array(annotation.shape)
+    remainder = annotation_shape[2] % 2
+    centre = annotation_shape // 2
     central_leftright_axis_annotations = annotation[centre[0], centre[1], :]
-    label_5_left_of_centre = central_leftright_axis_annotations[centre[2] + 5]
+    label_5_left_of_centre = central_leftright_axis_annotations[
+        centre[2] + (4 + remainder)
+    ]
     label_5_right_of_centre = central_leftright_axis_annotations[centre[2] - 5]
     assert (
         label_5_left_of_centre == label_5_right_of_centre
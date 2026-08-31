aaf688a8c3cd95b896f28cd4b71eeaff8626b3b6
kjungwoo5 <kjungwoo5@gmail.com>
2026-08-31 14:00:32 +0800

    Add hutchinson ferret to atlas_name.py

--- brainglobe_atlasapi/atlas_name.py
+++ brainglobe_atlasapi/atlas_name.py
@@ -139,6 +139,7 @@ AtlasName: TypeAlias = Literal[
     "drosophila_wingdisc_instar3_2um",
     "eurasian_blackcap_25um",
     "example_mouse_100um",
+    "hutchinson_ferret_500um",
     "kim_dev_mouse_e11-5_lsfm_20um",
     "kim_dev_mouse_e11-5_mri-adc_31.5um",
     "kim_dev_mouse_e11-5_mri-dwi_31.5um",

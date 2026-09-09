998f95c3cd99641dc47e0b551adbc07fa94a9f91
Igor Tatarnikov <61896994+IgorTatarnikov@users.noreply.github.com>
2026-09-09 13:54:14 +0100

    Remove unused references to gin.g-node.org (#958)
    * Removes extra test
    
    * Update comment for S3 bucket URL

--- brainglobe_atlasapi/descriptors.py
+++ brainglobe_atlasapi/descriptors.py
@@ -6,8 +6,7 @@ from typing import List, Tuple, TypeAlias
 import numpy as np
 import numpy.typing as npt
 
-# Base url of the gin repository:
-remote_url_base = "https://gin.g-node.org/brainglobe/atlases/raw/master/{}"
+# Base URL for the BrainGlobe atlas S3 bucket:
 remote_url_s3 = "s3://brainglobe/atlas/{}"
 remote_url_s3_http = "https://brainglobe.s3.us-west-2.amazonaws.com/atlas/{}"
 
